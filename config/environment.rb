21bcf77bdc19bca652bd38ffa7a139c882405340
jrgrs <jamie.rogers213@gmail.com>
2014-02-15 23:08:24 +0800

    emails

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Qfsproduce::Application.initialize!
+
+Qfsproduce::Application.configure do 
+	config.action_mailer.delivery_method = :smtp
+
+	config.action_mailer.smtp_settings = {
+		address: "smtp.gmail.com",
+		port: "587",
+		authentication: "plain",
+		user_name: "orders@qfsproduce.com",
+		password: "zsaxzsax",
+		enable_starttls_auto: true
+	}
+end
